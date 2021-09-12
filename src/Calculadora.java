f6772d4afcc6a3d06b406b9bd9640f8f6f1f7619
Aline4018 <aline12de@gmail.com>
2021-09-12 12:33:01 -0300

    Criando uma Calculadora de soma e multiplicação entre dois numeros

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,21 +2,41 @@ import java.util.Scanner;
 
 public class Calculadora {
     public static void main(String[] args) {
-        int numeroUm;
-        int numeroDois;
-        int multiplicacao;
+        double numeroUm;
+        double numeroDois;
+        double multiplicacao;
+        double soma;
+        int operacaoEscolhida;
 
         Scanner entradaTeclado = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro numero");
+        System.out.println("Bem vindo a calculadora, escolha a opção desejada");
+        System.out.println("Digite 1 para somar");
+        System.out.println("Digite 2 para multiplicar");
 
-        numeroUm = entradaTeclado.nextInt();
-        System.out.println("Digite o segundo numero");
-        numeroDois = entradaTeclado.nextInt();
+        operacaoEscolhida =  entradaTeclado.nextInt();
 
-        multiplicacao = numeroUm * numeroDois;
+        if (operacaoEscolhida == 1){
+            System.out.println("Digite o primeiro numero");
 
-        System.out.println("O resuldo da multiplicação de " + numeroUm + " * " + numeroDois + " = " + multiplicacao);
+            numeroUm =  entradaTeclado.nextDouble();
+            System.out.println("Digite o segundo numero");
+            numeroDois = entradaTeclado.nextDouble();
+
+            multiplicacao = numeroUm * numeroDois;
+
+            System.out.println("O resuldo da multiplicação de " + numeroUm + " * " + numeroDois + " = " + multiplicacao);
+
+        } else if (operacaoEscolhida == 2) {
+
+            System.out.println("Digite o primeiro numero");
+            numeroUm = entradaTeclado.nextDouble();
+            System.out.println("Digite o segundo numero");
+            numeroDois = entradaTeclado.nextDouble();
+
+            soma = numeroUm + numeroDois;
+            System.out.println("O resultado da soma de " + numeroUm + "+" + numeroDois + " = " + soma);
+        }
 
 
     }
